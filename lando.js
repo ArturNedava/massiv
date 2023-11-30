2e088d834ee35ddf9864b943b7025331eef32071
ArturNedava <121451856+ArturNedava@users.noreply.github.com>
2023-11-30 19:48:20 +0100

    First commit

---
+++ lando.js
@@ -0,0 +1,54 @@
+//1
+//  const friends = ["Mango" , " poly", "Ajax" , "Kiwi"]
+
+
+// const JoinMassiv = (arr) => {
+//     let string = ""
+//     for(let i = 0; i < arr.length; i++){
+//         string += arr[i]
+
+//         if(i < arr.length - 1){
+//             string += ","
+//         }
+//     }
+//     return arr;
+// }
+// const results = JoinMassiv(friends);
+// console.log(results);
+
+//0.1
+
+// const friends = ["Mango" , " poly", "Ajax" , "Kiwi"]
+
+// let string = ""
+
+// const joins = (arr) => {
+
+//     return arr.join(",");
+// }
+
+// const resultes = joins(friends)
+// console.log(resultes);
+
+//2
+
+// const cards = [
+// "какрточка - 1",
+// "какрточка - 2",
+// "какрточка - 3",
+// "какрточка - 4",
+// "какрточка - 5",
+// ];
+
+
+
+// cards.splice(2, 1);
+//console.log(cards);
+
+// cards.push("карточка 6")
+// console.log(cards)
+
+// cards[3] = "новая карточка";
+// console.log(cards);
+
+
